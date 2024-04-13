f1b336aea220a266611fbc967ca0b03310575ba3
João Gustavo <joao@exemplo.com>
2024-04-13 20:57:48 -0300

    js alterado

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ $(document).ready(function() {
 
     $('form').on('submit', function(e) {
         e.preventDefault();
-        const enderecoDaNovaImagem = $('#endereco-nova-imagem').val();
+        const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
         const novoItem = $('<li style="display: none"></li>');
         $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
         $(`
-            <div class="overlay-imagem-link">
+            <div class="#overlay-imagem-link">
                 <a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
                     Ver imagem em tamanho real 
                 </a>
